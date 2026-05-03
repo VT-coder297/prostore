558591d68f5898371590bf169490494fdd477d8a
VT-coder297 <tmot45548@gmail.com>
2026-05-03 15:14:46 +0000

    added search filtering, drawer & carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,25 @@
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 import ProductList from '@/components/shared/product/product-list';
 // import sampleData from '@/db/sample-data';
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from '@/lib/actions/product.actions';
+import ProductCarousel from '@/components/shared/product/product-carousel';
+import ViewAllProductsButton from '@/components/view-all-products-button';
 
 const Homepage = async () => {
   // await delay(2000);
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+      <ViewAllProductsButton />
     </>
   );
 };
